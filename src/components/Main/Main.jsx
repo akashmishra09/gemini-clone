c9ea919e60da327ddfcb98f4edceabfe5d320448
akashmishra09 <akash.mishra1202@gmail.com>
2024-10-24 21:03:52 +0530

    made app responsive

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -56,7 +56,17 @@ const Main = () => {
             </div>
             <div className="result-data">
               <img src={assets.gemini_icon} alt="" />
-              <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              {loading ? (
+                <>
+                  <div className="loader">
+                    <hr />
+                    <hr />
+                    <hr />
+                  </div>
+                </>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
             </div>
           </div>
         )}
@@ -74,13 +84,15 @@ const Main = () => {
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img
-                onClick={() => {
-                  onSent();
-                }}
-                src={assets.send_icon}
-                alt=""
-              />
+              {input ? (
+                <img
+                  onClick={() => {
+                    onSent();
+                  }}
+                  src={assets.send_icon}
+                  alt=""
+                />
+              ) : null}
             </div>
           </div>
           <p className="bottom-info">
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,9 +2,18 @@
 import React, { useState } from "react";
 import "./css/sidebar.css";
 import { assets } from "../../assets/assets";
+import { useContext } from "react";
+import { Context } from "../../context/Context";
 
 export const Sidebar = () => {
   const [extended, setExtended] = useState(false);
+  const { onSent, previousPrompts, setRecentPrompt, newChat } =
+    useContext(Context);
+
+  const loadPrompt = async (prompt) => {
+    setRecentPrompt(prompt);
+    await onSent(prompt);
+  };
 
   return (
     <div className="sidebar">
@@ -15,17 +24,22 @@ export const Sidebar = () => {
           src={assets.menu_icon}
           alt=""
         />
-        <div className="new-chat">
+        <div onClick={() => newChat()} className="new-chat">
           <img src={assets.plus_icon} alt="" />
           {extended ? <p>New Chat</p> : null}
         </div>
         {extended ? (
           <div className="recent">
             <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-              <img src={assets.message_icon} alt="" />
-              <p>What is react...</p>
-            </div>
+            {previousPrompts.map((item, index) => {
+              return (
+                // eslint-disable-next-line react/jsx-key
+                <div onClick={() => loadPrompt(item)} className="recent-entry">
+                  <img src={assets.message_icon} alt="" />
+                  <p>{item.slice(0, 18)}</p>
+                </div>
+              );
+            })}
           </div>
         ) : null}
       </div>
